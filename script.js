539693a38853461460f660453b38497161bb366d
Adriel <adriel.fariasan@gmail.com>
2022-02-17 18:42:37 -0300

    Adiciona funcao de voltar slide

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const btnVoltar = document.getElementById("btnVoltar");
 const imagens = [...document.querySelectorAll(".slider img")];
 
 const btnProx = document.getElementById("btnProx");
 btnProx.addEventListener("click", proxImagem);
+
+const btnVoltar = document.getElementById("btnVoltar");
+btnVoltar.addEventListener("click", voltarImagem);
 
 function proxImagem() {
   let encontrou = false;
@@ -22,5 +24,26 @@ function proxImagem() {
     imagens[0].classList.add("visivel");
   } else {
     imagens[indice + 1].classList.add("visivel");
+  }
+}
+
+function voltarImagem() {
+  let encontrou = false;
+  let indice = 0;
+
+  imagens.map((img, i) => {
+    if (!encontrou) {
+      if (img.classList.contains("visivel")) {
+        img.classList.remove("visivel");
+        encontrou = true;
+        indice = i;
+      }
+    }
+  });
+
+  if (indice === 0) {
+    imagens[imagens.length - 1].classList.add("visivel");
+  } else {
+    imagens[indice - 1].classList.add("visivel");
   }
 }
